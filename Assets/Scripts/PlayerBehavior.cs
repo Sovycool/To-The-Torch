46d659e6d72a7fa6229c714677fc72995a487c6b
Sovycool <sovann.boubou@gmail.com>
2024-05-17 21:51:05 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position += new Vector3(0, 10, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
